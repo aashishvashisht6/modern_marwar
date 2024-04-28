c3e48069a4d0b84cce75f98c1d1e52762d22bcca
aashishvashisht6 <aashishvashisht6@gmail.com>
2024-04-28 09:27:56 +0000

    feat: added barcode print doctype & print format

---
+++ modern_marwar_erp/modern_marwar_erp/doctype/barcode_print/barcode_print.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Aashish Vashisht and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Barcode Print", {
+// 	refresh(frm) {
+
+// 	},
+// });
