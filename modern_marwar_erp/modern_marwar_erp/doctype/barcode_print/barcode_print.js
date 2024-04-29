ea4da7a6c9f6c11126af5eb8011059b8217bba08
aashishvashisht6 <aashishvashisht6@gmail.com>
2024-04-29 20:26:09 +0000

    fix: barcode print doctype & print format

--- modern_marwar_erp/modern_marwar_erp/doctype/barcode_print/barcode_print.js
+++ modern_marwar_erp/modern_marwar_erp/doctype/barcode_print/barcode_print.js
@@ -1,8 +1,32 @@
 // Copyright (c) 2024, Aashish Vashisht and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Barcode Print", {
-// 	refresh(frm) {
+frappe.ui.form.on("Barcode Print", {
+    item_code(frm) {
+        if (frm.doc.item_code) {
+            frappe.db.get_doc("Item", frm.doc.item_code).then(resp => {
+                const color_row = resp?.attributes?.filter(row => row.attribute === "Colour" || row.attribute === "Color")
+                if (color_row.length > 0) {
+                    frm.set_value("colour", color_row[0].attribute_value)
+                }
+            })
 
-// 	},
-// });
+        }
+    },
+    size: frm => {
+        if (frm.doc.size) {
+            frm.set_df_property("meter", "hidden", 1)
+        }
+        else {
+            frm.set_df_property("meter", "hidden", 0)
+        }
+    },
+    meter: frm => {
+        if (frm.doc.meter) {
+            frm.set_df_property("size", "hidden", 1)
+        }
+        else {
+            frm.set_df_property("size", "hidden", 0)
+        }
+    }
+});
